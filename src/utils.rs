3b7807cf5ec9fc505831a8ea9023851b2e5e87c7
gstpsk <gstpsk@proton.me>
2026-03-23 13:55:51 +0100

    Make get_random_position return a u32 tuple

--- src/utils.rs
+++ src/utils.rs
@@ -1,14 +1,14 @@
 use image::GenericImageView;
 
 // returns a random valid (x, y) pair
-pub fn get_random_position(surface_width: i32, surface_height: i32) -> (i32, i32) {
-    let mut result: (i32, i32) = (0 ,0);
+pub fn get_random_position(surface_width: u32, surface_height: u32) -> (u32, u32) {
+    let mut result: (u32, u32) = (0 ,0);
     
-    result.0 = rand::random::<i32>() % surface_width;
-    result.1 = rand::random::<i32>() % surface_height;
+    result.0 = rand::random::<u32>() % surface_width;
+    result.1 = rand::random::<u32>() % surface_height;
 
-    result.0 = result.0.abs();
-    result.1 = result.1.abs();
+    //result.0 = result.0.abs();
+    //result.1 = result.1.abs();
 
     result
 }
