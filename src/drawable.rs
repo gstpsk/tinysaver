be01e09834dcb5aa6b3f94a658e89377933a9af7
gstpsk <gstpsk@proton.me>
2026-04-01 01:24:40 +0200

    Add Shape::Line to Drawable and add set_rotation()

--- src/drawable.rs
+++ src/drawable.rs
@@ -5,19 +5,22 @@ pub enum Shape {
     Rectangle { 
         width: f32, 
         height: f32 
-    }
+    },
+    Line { dx: f32, dy: f32, thickness: f32 }
 }
 
 impl Shape {
     pub fn width(&self) -> f32 {
         match *self {
             Shape::Rectangle { width, .. } => width,
+            Shape::Line { dx, .. } => dx,
         }
     }
 
     pub fn height(&self) -> f32 {
         match *self {
             Shape::Rectangle { height, .. } => height,
+            Shape::Line { dy, .. } => dy
         }
     }
 }
@@ -50,15 +53,23 @@ impl Drawable {
         alpha: u8,
         material: Material
     ) -> Self {
-        let (scale_x, scale_y) = match shape {
-            Shape::Rectangle { width, height } => (width, height),
-        };
+        // default values
+        let (mut rotation, scale_x, scale_y) = (0.0, 1.0, 1.0);
+        match shape {
+            Shape::Rectangle { .. } => {
+                // default values
+                //let (rotation, scale_x, scale_y) = (0.0, 1.0, 1.0);
+            },
+            Shape::Line { dx, dy, .. } => {
+                rotation = dy.atan2(dx);
+            },
+        }
 
         Self {
             shape,
             x,
             y,
-            rotation: 0.0,
+            rotation,
             scale_x,
             scale_y,
             color,
@@ -74,6 +85,10 @@ impl Drawable {
     pub fn set_position(&mut self, x: f32, y: f32) {
         self.x = x;
         self.y = y;
+    }
+
+    pub fn set_rotation(&mut self, rotation: f32) {
+        self.rotation = rotation;
     }
 
     pub fn set_scale(&mut self, scale_x: f32, scale_y: f32) {
@@ -94,6 +109,7 @@ impl Drawable {
     pub fn shape_type(&self) -> u32 {
         match self.shape {
             Shape::Rectangle { .. } => 0,
+            Shape::Line { .. } => 1,
         }
     }
 
@@ -105,18 +121,41 @@ impl Drawable {
     }
 
     pub fn to_instance_data(&self) -> InstanceData {
-        InstanceData {
-            position: [self.x, self.y],
-            scale: [self.scale_x, self.scale_y],
-            rotation: self.rotation,
-            color: [
-                self.color.0 as f32 / 255.0,
-                self.color.1 as f32 / 255.0,
-                self.color.2 as f32 / 255.0,
-                self.alpha as f32 / 255.0,
-            ],
-            shape_type: self.shape_type(),
-            texture_index: self.texture_index(),
+        match self.shape {
+            Shape::Rectangle { width, height } => {
+                InstanceData {
+                    position: [self.x, self.y],
+                    scale: [width * self.scale_x, height * self.scale_y],
+                    rotation: self.rotation,
+                    color: [
+                        self.color.0 as f32 / 255.0,
+                        self.color.1 as f32 / 255.0,
+                        self.color.2 as f32 / 255.0,
+                        self.alpha as f32 / 255.0,
+                    ],
+                    shape_type: self.shape_type(),
+                    texture_index: self.texture_index(),
+                }
+
+            },
+            Shape::Line { dx, dy, thickness } => {
+                let width = (dx*dx + dy*dy).sqrt();
+                let height = thickness;
+                InstanceData {
+                    position: [self.x, self.y],
+                    scale: [width * self.scale_x, height * self.scale_y],
+                    rotation: self.rotation,
+                    color: [
+                        self.color.0 as f32 / 255.0,
+                        self.color.1 as f32 / 255.0,
+                        self.color.2 as f32 / 255.0,
+                        self.alpha as f32 / 255.0,
+                    ],
+                    shape_type: self.shape_type(),
+                    texture_index: self.texture_index(),
+                }
+
+            }
         }
     }
 }
