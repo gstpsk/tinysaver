2a627b0686712283c612475efd515616cabc644d
gstpsk <gstpsk@proton.me>
2026-04-01 01:24:00 +0200

    Add on_key() method to Animation trait

--- src/animation.rs
+++ src/animation.rs
@@ -3,4 +3,5 @@ use wgpu;
 pub trait Animation {
     fn update(&mut self, queue: &wgpu::Queue);
     fn render(&self, queue: &wgpu::Queue, encoder: &mut wgpu::CommandEncoder, target: &wgpu::TextureView);
+    fn on_key(&mut self, key: winit::keyboard::Key);
 }
